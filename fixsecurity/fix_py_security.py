e9d4153c72b764a32ade25642bbca49ac9acb91a
freeman lo <flo@radford.edu>
2023-04-05 15:14:37 -0400

    Fixed Security

---
+++ fixsecurity/fix_py_security.py
@@ -0,0 +1,11 @@
+"""
+"""
+
+def main():
+    a = int(input("Enter in a number? ").strip())
+    b = int(input("Enter in a number? ").strip())
+    ans = a + b
+    print(ans)
+
+if __name__ == "__main__":
+    main()
