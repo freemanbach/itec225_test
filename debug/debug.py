41bb6820b83331f1edc703b7cfedcea48cf2051c
freeman lo <flo@radford.edu>
2023-04-04 23:24:20 -0400

    added debug

---
+++ debug/debug.py
@@ -0,0 +1,11 @@
+
+
+def main():
+
+    ui1 = int(input("Enter in a number? ").strip())
+    ui2 = int(input("Enter in a number? ").strip())
+    ans = ui1 + ui2
+    print("Your answer is {}".format(ans))
+
+if __name__ == "__main__":
+    main()
